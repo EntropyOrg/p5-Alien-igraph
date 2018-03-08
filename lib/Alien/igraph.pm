73263320d9cb112600d8be172af03f88cf298748
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-08 09:11:41 -0500

    Use static linking
    This allows for linking on macOS.

--- lib/Alien/igraph.pm
+++ lib/Alien/igraph.pm
@@ -9,6 +9,30 @@ sub inline_auto_include {
 	return  [ 'igraph.h' ];
 }
 
+sub Inline {
+	my ($self, $lang) = @_;
+
+	if('C') {
+		my $params = Alien::Base::Inline(@_);
+
+		# Use static linking instead of dynamic linking. This works
+		# better on some platforms. On macOS, to use dynamic linking,
+		# the `install_name` of the library must be set, but since this
+		# is the final path by default, linking to the `.dylib` under
+		# `blib/` at test time does not work without using `@rpath`.
+		$params->{MYEXTLIB} .= ' ' .
+			join( " ",
+				map { File::Spec->catfile(
+					File::Spec->rel2abs(Alien::igraph->dist_dir),
+					'lib',  $_ ) }
+				qw(libigraph.a)
+			);
+		$params->{LIBS} =~ s/-ligraph//g;
+		return $params;
+	}
+}
+
+
 1;
 
 __END__
